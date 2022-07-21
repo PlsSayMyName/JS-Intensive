42dd6bec90dd6316fb37212a3080829010eb251d
Dmitrij Menkowskij <madaraisback@gmail.com>
2022-07-21 12:38:55 +0200

    complete task-7

---
+++ task-7.js
@@ -0,0 +1,12 @@
+function createDebounceFunction(func, timeout) {
+    let timer;
+    return (...args) => {
+        if (!timer) {
+            func.apply(this, args);
+        }
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            timer = undefined;
+        }, timeout);
+    };
+}
